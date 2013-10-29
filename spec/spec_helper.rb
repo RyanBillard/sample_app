15abe1a97c9636b58417c34626216e124f4afa54
Ryan Billard <ryanagbillard@gmail.com>
2013-10-29 11:16:10 -0400

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #adding Capybara DSL to RSpec helper file
+  config.include Capybara::DSL
 end
